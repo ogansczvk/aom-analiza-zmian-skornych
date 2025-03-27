73f8cc49125eb4b3d608fd31a4a42a28bf9b1abe
ogansczyk <oliwia6543217@gmail.com>
2025-03-27 23:37:53 +0100

    Dodanie morfologii blackhat i tophat + testy

--- hairDetection.py
+++ hairDetection.py
@@ -3,7 +3,7 @@ import cv2
 #IMAGE ACQUISITION
 
 #Input image
-path='ISIC_0024323.jpg'
+path='C:/Users/oliwi/OneDrive/Pulpit/Studia/semestr 6/HAM10000_images_part_1/ISIC_0024320.jpg'
 #Read image
 image=cv2.imread(path,cv2.IMREAD_COLOR)
 #Image cropping
@@ -14,22 +14,29 @@ img=image[30:410,30:560]
 #Gray scale
 grayScale = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY )
 #Black hat filter - wykrywanie ciemnych obiektów
-kernel = cv2.getStructuringElement(1,(9,9)) # maska o wymiarach 9x9
+kernel = cv2.getStructuringElement(1,(15,15)) # maska o wymiarach 9x9 #1 oznacza krzyżowy element strukturalny, 0 -prostokatny, 2-eliptyczny
 blackhat = cv2.morphologyEx(grayScale, cv2.MORPH_BLACKHAT, kernel)
+tophat = cv2.morphologyEx(blackhat, cv2.MORPH_TOPHAT, kernel)
+
 #Gaussian filter
-bhg= cv2.GaussianBlur(blackhat,(3,3),cv2.BORDER_DEFAULT)
+bhg= cv2.GaussianBlur(tophat,(3,3),cv2.BORDER_DEFAULT)
+#bhg_blackhat= cv2.GaussianBlur(blackhat,(3,3),cv2.BORDER_DEFAULT) #tu musza byc wartosci nieparzyste, przy zwiekszeniu do (9,9) daje gorsze efekty
 #Binary thresholding (MASK) Jeśli piksel ma wartość większą niż 10, staje się białym (255), w przeciwnym razie czarnym (0).
 ret,mask = cv2.threshold(bhg,10,255,cv2.THRESH_BINARY)
+#ret_blackhat,mask_blackhat = cv2.threshold(bhg_blackhat,10,255,cv2.THRESH_BINARY)
 #Replace pixels of the mask - Metoda "inpainting" zastępuje obszary z włosami pobliskimi pikselami skóry.
 dst = cv2.inpaint(img,mask,6,cv2.INPAINT_TELEA)
+#dst_blackhat = cv2.inpaint(img,mask_blackhat,6,cv2.INPAINT_TELEA)
 
 #Display images
 cv2.imshow("Original image",image)
-cv2.imshow("Cropped image",img)
-cv2.imshow("Gray Scale image",grayScale)
-cv2.imshow("Blackhat",blackhat)
-cv2.imshow("Binary mask",mask)
-cv2.imshow("Clean image",dst)
+#cv2.imshow("Cropped image",img)
+#cv2.imshow("Gray Scale image",grayScale)
+#cv2.imshow("tophat",tophat)
+#cv2.imshow("blackhat", blackhat)
+#cv2.imshow("Binary mask",mask)
+cv2.imshow("Clean image tophat",dst)
+#cv2.imshow("Clean image",dst_blackhat)
 
 cv2.waitKey()
-cv2.destroyAllWindows()
+#cv2.destroyAllWindows()
