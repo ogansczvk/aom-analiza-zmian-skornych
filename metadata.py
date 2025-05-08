7979c5b1942eee279e3bea8887602afe24545523
ogansczyk <oliwia6543217@gmail.com>
2025-05-08 11:36:50 +0200

    wyciaganie zdjec na podsatwie pliku csv

--- metadata.py
+++ metadata.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import os
+import pickle
+
+
+photos = []
 
 zdjecia = './zdjecia'
 files_in_folder = os.listdir(zdjecia)
@@ -10,10 +14,16 @@ for index, row in df.iterrows():
     kolumna0 = row[1] #nr zdjecia
     kolumna2 = row[2] #choroba
     kolumna3 = row[3] #sposob badania
-    if (kolumna2 == 'mel') and (kolumna3 == 'histo'):
+    if (kolumna2 == 'mel') and (kolumna3 == 'histo'): #mel ma tylko histo
         #print(kolumna0)
         for filename in files_in_folder:
             if kolumna0 in filename:
+                photo_path = os.path.join(zdjecia, filename)
+                photos.append(photo_path)
                 count += 1
+                photo = kolumna0
                 print(count)
+
+with open("photos.pkl", "wb") as f:
+    pickle.dump(photos, f)
         
